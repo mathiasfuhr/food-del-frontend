4c53c6423ea82a43d79dcb72fb98acf2f635e6df
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-08-05 23:13:12 -0300

    first commit

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,39 @@
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+        provident autem itaque repellendus neque culpa reprehenderit,
+        distinctio, nisi veniam accusamus vel ex, voluptatum laborum numquam
+        quibusdam eveniet error! Odio, repudiandae.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => (
+          <div
+            onClick={() =>
+              setCategory((prev) =>
+                prev === item.menu_name ? "All" : item.menu_name
+              )
+            }
+            key={index}
+            className="explore-menu-list-item"
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt={`Image for ${item.menu_name}`} // Melhorar acessibilidade
+            />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your fovourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          sapiente saepe, deleniti illum vero minima quae aperiam hic,
+          aspernatur labore, provident ea eius dolorem ut nemo repellat numquam
+          corporis voluptatum.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
